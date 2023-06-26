c1b3dfa8d273c81ace2976fc87b26240a521afb3
vioo-bkp <59666442+vioo-bkp@users.noreply.github.com>
2023-06-26 17:34:54 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace DimGlow
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            trackBar1 = new TrackBar();
+            notifyIcon1 = new NotifyIcon(components);
+            textBox1 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(94, 349);
+            button1.Name = "button1";
+            button1.Size = new Size(131, 40);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(323, 349);
+            button2.Name = "button2";
+            button2.Size = new Size(131, 40);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(600, 349);
+            button3.Name = "button3";
+            button3.Size = new Size(131, 40);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(244, 133);
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(299, 80);
+            trackBar1.TabIndex = 3;
+            trackBar1.Scroll += trackBar1_Scroll;
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.Text = "notifyIcon1";
+            notifyIcon1.Visible = true;
+            notifyIcon1.MouseDoubleClick += notifyIcon1_MouseDoubleClick;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(297, 206);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(175, 35);
+            textBox1.TabIndex = 4;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(trackBar1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private TrackBar trackBar1;
+        private NotifyIcon notifyIcon1;
+        private TextBox textBox1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DimGlow
+{
+    public partial class Form1 : Form
+    {
+        private OverlayForm overlayForm;
+        private NotifyIcon notifyIcon;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Initialize the overlay form
+            overlayForm = new OverlayForm
+            {
+                Visible = false
+            };
+
+            Text = "DimGlow";
+            //
+            // trackBar functionality
+            //
+            trackBar1.Minimum = 0;
+            trackBar1.Maximum = 95;
+            trackBar1.Value = Properties.Settings.Default.Transparency;
+            trackBar1.TickFrequency = 10;
+            trackBar1.LargeChange = 10;
+            trackBar1.SmallChange = 1;
+            trackBar1.Scroll += new EventHandler(trackBar1_Scroll);
+            //
+            // textBox functionality
+            // 
+            textBox1.Text = trackBar1.Value.ToString("Insert Value");
+            textBox1.TextChanged += new EventHandler(textBox1_TextChanged);
+            //
+            // icon functionality
+            //
+            this.ShowInTaskbar = true;
+            this.Icon = new Icon("images\\icon.ico");
+            ApplyDarkOverlay(trackBar1.Value);
+
+        }
+        private void ApplyDarkOverlay(int transparency)
+        {
+            // Set overlay form opacity based on transparency
+            overlayForm.Opacity = transparency / 100.0;
+            overlayForm.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Reset button for the trackbar and the overlay
+            trackBar1.Value = trackBar1.Minimum;
+            textBox1.Text = trackBar1.Value.ToString();
+            overlayForm.Visible = false;
+            ApplyDarkOverlay(trackBar1.Value);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Properties.Settings.Default.Transparency = trackBar1.Value;
+            Properties.Settings.Default.Save();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // minimize button - system tray
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Text = "DimGlow";
+            notifyIcon.Icon = new Icon("images\\icon.ico");
+            notifyIcon.Visible = true;
+            notifyIcon.DoubleClick += new EventHandler(notifyIcon_DoubleClick);
+            this.ShowInTaskbar = false;
+            this.Hide();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Parse the TextBox value and update the trackbar value
+            if (int.TryParse(textBox1.Text, out int value))
+            {
+                // Enforce the two-digit restriction
+                if (textBox1.Text.Length > 2)
+                {
+                    textBox1.Text = textBox1.Text.Substring(0, 2);
+                    textBox1.Select(2, 0);
+                }
+
+                if (value >= trackBar1.Minimum && value <= trackBar1.Maximum)
+                {
+                    trackBar1.Value = value;
+                    ApplyDarkOverlay(value);
+                }
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            int transparency = trackBar1.Value;
+            ApplyDarkOverlay(transparency);
+            textBox1.Text = trackBar1.Value.ToString();
+        }
+
+        void notifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+            notifyIcon.Visible = false;
+        }
+
+    }
+    public class OverlayForm : Form
+    {
+        private const int WS_EX_TRANSPARENT = 0x20;
+        private const int WS_EX_LAYERED = 0x80000;
+
+        public OverlayForm()
+        {
+            InitializeComponent();
+
+            // Set form properties
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            this.TopMost = true;
+            this.DoubleBuffered = true;
+            this.BackColor = Color.Black;
+            this.ShowInTaskbar = false;
+            this.Opacity = 0.1;
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            // Do not paint background to reduce flicker
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= WS_EX_TRANSPARENT | WS_EX_LAYERED;
+                return cp;
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // OverlayForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "OverlayForm";
+            this.Text = "OverlayForm";
+            this.ResumeLayout(false);
+        }
+    }
+}
